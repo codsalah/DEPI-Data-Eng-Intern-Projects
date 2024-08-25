6f8d9fc78bf7031b0397fed8ca66f86d2ae76ce9
salah mohamed <103036456+codsalah@users.noreply.github.com>
2024-08-25 10:37:29 +0300

    Delete spark_project directory

--- spark_project/pyspark_tasks/data_writer.py
+++
@@ -1,6 +0,0 @@
-import os 
-def save_to_csv(df, path):
-    if not os.path.exists(os.path.dirname(path)):
-        os.makedirs(os.path.dirname(path))
-
-    df.write.mode('overwrite').csv(path, header=True)
